f1157a3e18e9eac399a079d227ecea006660c9f1
Gonzalolog <gonzalolog@gmail.com>
2024-07-25 15:39:28 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace MKSCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            previewImage = new PictureBox();
+            targetPath = new TextBox();
+            label1 = new Label();
+            sizex = new TextBox();
+            label2 = new Label();
+            kindseq = new RadioButton();
+            kindsingle = new RadioButton();
+            label3 = new Label();
+            label4 = new Label();
+            sizey = new TextBox();
+            exportButton = new Button();
+            sdkpathLabel = new LinkLabel();
+            additiveCheckbox = new CheckBox();
+            blendBox = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)previewImage).BeginInit();
+            SuspendLayout();
+            // 
+            // previewImage
+            // 
+            previewImage.Location = new Point(12, 90);
+            previewImage.Name = "previewImage";
+            previewImage.Size = new Size(300, 300);
+            previewImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            previewImage.TabIndex = 0;
+            previewImage.TabStop = false;
+            previewImage.DragDrop += previewImage_DragDrop;
+            previewImage.Paint += previewImage_Paint;
+            // 
+            // targetPath
+            // 
+            targetPath.Location = new Point(12, 41);
+            targetPath.Name = "targetPath";
+            targetPath.Size = new Size(300, 31);
+            targetPath.TabIndex = 1;
+            targetPath.TextChanged += targetPath_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(152, 25);
+            label1.TabIndex = 2;
+            label1.Text = "$basetexture Path";
+            // 
+            // sizex
+            // 
+            sizex.Location = new Point(318, 224);
+            sizex.Name = "sizex";
+            sizex.Size = new Size(136, 31);
+            sizex.TabIndex = 4;
+            sizex.Text = "2";
+            sizex.TextChanged += rowColChange;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(318, 90);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 25);
+            label2.TabIndex = 5;
+            label2.Text = "Type";
+            // 
+            // kindseq
+            // 
+            kindseq.AutoSize = true;
+            kindseq.Checked = true;
+            kindseq.Location = new Point(318, 118);
+            kindseq.Name = "kindseq";
+            kindseq.Size = new Size(113, 29);
+            kindseq.TabIndex = 6;
+            kindseq.TabStop = true;
+            kindseq.Text = "Sequence";
+            kindseq.UseVisualStyleBackColor = true;
+            // 
+            // kindsingle
+            // 
+            kindsingle.AutoSize = true;
+            kindsingle.Location = new Point(318, 153);
+            kindsingle.Name = "kindsingle";
+            kindsingle.Size = new Size(85, 29);
+            kindsingle.TabIndex = 7;
+            kindsingle.Text = "Single";
+            kindsingle.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(318, 196);
+            label3.Name = "label3";
+            label3.Size = new Size(82, 25);
+            label3.TabIndex = 8;
+            label3.Text = "Columns";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(318, 258);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 25);
+            label4.TabIndex = 9;
+            label4.Text = "Rows";
+            // 
+            // sizey
+            // 
+            sizey.Location = new Point(318, 286);
+            sizey.Name = "sizey";
+            sizey.Size = new Size(136, 31);
+            sizey.TabIndex = 10;
+            sizey.Text = "2";
+            sizey.TextChanged += rowColChange;
+            // 
+            // exportButton
+            // 
+            exportButton.Location = new Point(12, 404);
+            exportButton.Name = "exportButton";
+            exportButton.Size = new Size(440, 34);
+            exportButton.TabIndex = 11;
+            exportButton.Text = "Export";
+            exportButton.UseVisualStyleBackColor = true;
+            exportButton.Click += button2_Click;
+            // 
+            // sdkpathLabel
+            // 
+            sdkpathLabel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            sdkpathLabel.AutoSize = true;
+            sdkpathLabel.Location = new Point(370, 13);
+            sdkpathLabel.Name = "sdkpathLabel";
+            sdkpathLabel.Size = new Size(84, 25);
+            sdkpathLabel.TabIndex = 12;
+            sdkpathLabel.TabStop = true;
+            sdkpathLabel.Text = "SDK Path";
+            sdkpathLabel.TextAlign = ContentAlignment.TopRight;
+            sdkpathLabel.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // additiveCheckbox
+            // 
+            additiveCheckbox.AutoSize = true;
+            additiveCheckbox.Location = new Point(318, 323);
+            additiveCheckbox.Name = "additiveCheckbox";
+            additiveCheckbox.Size = new Size(104, 29);
+            additiveCheckbox.TabIndex = 13;
+            additiveCheckbox.Text = "Additive";
+            additiveCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // blendBox
+            // 
+            blendBox.AutoSize = true;
+            blendBox.Location = new Point(318, 358);
+            blendBox.Name = "blendBox";
+            blendBox.Size = new Size(144, 29);
+            blendBox.TabIndex = 14;
+            blendBox.Text = "Blend Frames";
+            blendBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AllowDrop = true;
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(464, 452);
+            Controls.Add(blendBox);
+            Controls.Add(additiveCheckbox);
+            Controls.Add(sdkpathLabel);
+            Controls.Add(exportButton);
+            Controls.Add(sizey);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(kindsingle);
+            Controls.Add(kindseq);
+            Controls.Add(label2);
+            Controls.Add(sizex);
+            Controls.Add(label1);
+            Controls.Add(targetPath);
+            Controls.Add(previewImage);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "SpriteCard Edit";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)previewImage).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox previewImage;
+        private TextBox targetPath;
+        private Label label1;
+        private TextBox sizex;
+        private Label label2;
+        private RadioButton kindseq;
+        private RadioButton kindsingle;
+        private Label label3;
+        private Label label4;
+        private TextBox sizey;
+        private Button exportButton;
+        private LinkLabel sdkpathLabel;
+        private CheckBox additiveCheckbox;
+        private CheckBox blendBox;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+using System;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using TGASharpLib;
+
+namespace MKSCreator
+{
+    public partial class Form1 : Form
+    {
+        int frameCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+
+            string lastImage = Properties.Settings.Default.lastImage;
+            if (lastImage != "")
+            {
+                previewImage.ImageLocation = lastImage;
+            }
+
+            string lastPath = Properties.Settings.Default.lastPath;
+            if (lastPath != "")
+            {
+                targetPath.Text = lastPath;
+            }
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string file = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            if (file.EndsWith(".png") || file.EndsWith(".jpg") || file.EndsWith(".jpeg"))
+            {
+                Properties.Settings.Default.lastImage = file;
+                Properties.Settings.Default.Save();
+
+                previewImage.ImageLocation = file;
+            }
+            else
+            {
+                MessageBox.Show("Please drop a valid image file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = Properties.Settings.Default.mksPath;
+            if (path == "")
+            {
+                MessageBox.Show("Please you have to provide mksheet.exe path", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //Open a save file dialog for a vtf file to save
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "VTF File|*.vtf";
+            saveFileDialog1.Title = "Save the VTF file";
+            saveFileDialog1.ShowDialog();
+
+            string vtfName = saveFileDialog1.FileName;
+            string vmtName = vtfName.Replace(".vtf", ".vmt");
+            string fileNameNoExtension = Path.GetFileNameWithoutExtension(vtfName);
+            string folder = Path.GetDirectoryName(vtfName);
+
+            generateVMTFile(fileNameNoExtension, vmtName);
+            createWorkSpace(folder, vtfName, fileNameNoExtension);
+            generateMKS(folder, fileNameNoExtension);
+        }
+
+        void createWorkSpace(string folder, string vtf, string fileNoName)
+        {
+            if (Directory.Exists(folder + "\\" + fileNoName))
+                Directory.Delete(folder + "\\" + fileNoName, true);
+
+            Directory.CreateDirectory(folder + "\\" + fileNoName);
+            Bitmap image = new Bitmap(previewImage.ImageLocation);
+            float cols = Int32.Parse(sizex.Text);
+            float rows = Int32.Parse(sizey.Text);
+            int colSize = (int)Math.Floor(image.Width / cols);
+            int rowSize = (int)Math.Floor(image.Height / rows);
+
+            int frameId = 0;
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    Bitmap newBitmap = new Bitmap(colSize, rowSize);
+                    Graphics g = Graphics.FromImage(newBitmap);
+                    g.DrawImage(image, new Rectangle(0, 0, colSize, rowSize), new Rectangle(x * colSize, y * rowSize, colSize, rowSize), GraphicsUnit.Pixel);
+
+                    TGA tgaImage = TGA.FromBitmap(newBitmap);
+                    tgaImage.Save(folder + "\\" + fileNoName + "\\" + frameId + ".tga");
+                    frameId++;
+                }
+            }
+
+            frameCount = frameId;
+        }
+
+        void generateVMTFile(string fileNameNoExtension, string vmt)
+        {
+            string vmtTemplate = "\"SpriteCard\"\n" +
+                "{\n" +
+                "\t$basetexture \"" + targetPath.Text + "/" + fileNameNoExtension + "\"\n" +
+                "\t$translucent \"1\"\n" +
+                "\t$blendframes \"" + (blendBox.Checked ? "1" : "0") + "\"\n" +
+                "\t$additive \"" + (additiveCheckbox.Checked ? "1" : "0") + "\"\n}";
+
+            File.WriteAllText(vmt, vmtTemplate);
+        }
+
+        string workingDirectory;
+        void generateMKS(string folder, string basePath)
+        {
+            string filePath = folder + "\\" + basePath + "\\";
+            string content = "sequence 0";
+            if (kindseq.Checked) {
+                content += "\nloop\n";
+
+                for (int i = 0; i < frameCount; i++)
+                {
+                    content += "\nframe " + filePath + i + ".tga" + " 1 ";
+                }
+            } else
+            {
+                content += "\nloop";
+                for (int i = 0; i < frameCount; i++)
+                {
+                    content += "\nframe " + filePath + i + ".tga" + " 1\nsequence " + i + "\nloop\n";
+                }
+            }
+
+            string fileName = basePath + ".mks";
+            string mksPath = Properties.Settings.Default.mksPath;
+            string vtexPath = mksPath.Replace("mksheet.exe", "vtex.exe");
+            string batchFile = "@ECHO ON\n\"" + Properties.Settings.Default.mksPath + "\" \"" + filePath + fileName + "\"\n" +
+                "\"" + vtexPath + "\" -dontusegamedir -quiet " + filePath + "\\" + basePath + ".sht\n" +
+                "xcopy /y \"" + filePath + "\\" + basePath + ".vtf\"" + " \"" + folder + "\"\n" +
+                "rmdir " + filePath + " /s /q\nexit";
+            File.WriteAllText(filePath + fileName, content);
+            File.WriteAllText(folder + "\\mksheet.bat", batchFile);
+
+            //Run the last created bat file
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = folder + "\\mksheet.bat";
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.UseShellExecute = false;
+            workingDirectory = folder + "\\mksheet.bat";
+
+            Process.Start(processStartInfo);
+            Task.Run(() => removeFile());
+        }
+
+        private async void removeFile()
+        {
+            await Task.Delay(2000);
+            File.Delete(workingDirectory);
+        }
+
+        void batch_Exited(object sender, EventArgs e)
+        {
+            Directory.Delete(workingDirectory, true);
+        }
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenFileDialog fileSelector = new OpenFileDialog();
+            fileSelector.Filter = "MKSheet on bin folder|mksheet.exe";
+            fileSelector.Title = "Select the mksheet.exe file";
+            fileSelector.ShowDialog();
+
+            if (fileSelector.FileName == "")
+            {
+                return;
+            }
+
+            sdkpathLabel.Text = "SDK Path (OK)";
+            Properties.Settings.Default.mksPath = fileSelector.FileName;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = Properties.Settings.Default.mksPath;
+            if (path == "")
+            {
+                sdkpathLabel.Text = "SDK Path (Not set)";
+            }
+            else
+            {
+                sdkpathLabel.Text = "SDK Path (OK)";
+            }
+            sdkpathLabel.TextAlign = ContentAlignment.TopRight;
+        }
+
+        private void previewImage_DragDrop(object sender, DragEventArgs e)
+        {
+            MessageBox.Show("Dropped");
+        }
+
+        private void previewImage_Paint(object sender, PaintEventArgs e)
+        {
+            float cols = Int32.Parse(sizex.Text);
+            float rows = Int32.Parse(sizey.Text);
+            int colSize = (int)Math.Floor(300 / cols);
+            int rowSize = (int)Math.Floor(300 / rows);
+
+            using (Pen pen = new Pen(Color.Black, 1))
+                e.Graphics.DrawRectangle(pen, 0, 0, 299, 299);
+
+            for (int i = 1; i < cols; i++)
+                using (Pen pen = new Pen(Color.HotPink, 1))
+                    e.Graphics.DrawLine(pen, new Point(i * colSize, 0), new Point(i * colSize, 300));
+
+            for (int i = 1; i < rows; i++)
+                using (Pen pen = new Pen(Color.HotPink, 1))
+                    e.Graphics.DrawLine(pen, new Point(0, i * rowSize), new Point(300, i * rowSize));
+        }
+
+        private void rowColChange(object sender, EventArgs e)
+        {
+            TextBox self = (TextBox)sender;
+            int result;
+            if (Int32.TryParse(self.Text, out result))
+            {
+                previewImage.Invalidate();
+            }
+        }
+
+        private void targetPath_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.lastPath = targetPath.Text;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
